45776455e26beed730b5afefb2e12b27975717df
Travis James <travis@tribehealthsolutions.com>
2023-04-19 04:26:17 -0500

    post

--- pack_build.sh
+++ pack_build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-GOOS=linux GOARCH=amd64 pack build func-testfunction-go --builder openfunction/builder-go:v2.4.0-1.17 --env FUNC_NAME="TestFunction"  --env FUNC_CLEAR_SOURCE=true
+GOOS=linux GOARCH=amd64 pack build "tribehealth/test-function:v2" --builder openfunction/builder-go:v2.4.0-1.17 --env FUNC_NAME="TestFunction"  --env FUNC_CLEAR_SOURCE=true
--- test_function.go
+++ test_function.go
@@ -1,21 +1,20 @@
 package test_function
 
 import (
-	"fmt"
-	ofctx "github.com/OpenFunction/functions-framework-go/context"
+	"encoding/json"
 	"github.com/OpenFunction/functions-framework-go/functions"
 	"net/http"
 )
 
 func init() {
 	functions.HTTP("TestFunction", TestFunction,
-		functions.WithFunctionPath("/{echo}"))
+		functions.WithFunctionMethods("POST"))
 }
 
 func TestFunction(w http.ResponseWriter, r *http.Request) {
-	vars := ofctx.VarsFromCtx(r.Context())
-	_, err := fmt.Fprintf(w, "Echo: %s\n", vars["echo"])
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+	responseBytes, _ := json.Marshal(map[string]string{
+		"test": "test",
+	})
+	w.Header().Set("Content-type", "application/json")
+	w.Write(responseBytes)
 }
